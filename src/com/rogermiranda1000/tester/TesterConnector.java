baea4d89794c9a78c625b18df8afa09a6ae64db0
roger.miranda <roger.miranda@students.salle.url.edu>
2022-06-11 16:00:29 +0200

    Feature: start request 1/2

--- src/com/rogermiranda1000/tester/TesterConnector.java
+++ src/com/rogermiranda1000/tester/TesterConnector.java
@@ -13,29 +13,64 @@ import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 public class TesterConnector implements ServerManagerPetition {
+    public interface ArrayAdder { public void addToArray(ArrayList<Byte> out, Object []file); }
+
     private final Socket serversManagerSocket;
 
     public TesterConnector(Socket serversManagerSocket) {
         this.serversManagerSocket = serversManagerSocket;
     }
 
+    /* HELPER FUNCTIONS */
     private static byte []toByteArray(ArrayList<Byte> bytes) {
         byte []r = new byte[bytes.size()];
         for (int x = 0; x < r.length; x++) r[x] = bytes.get(x);
         return r;
     }
 
+    /**
+     * Add a byte[] directly to the socket
+     */
+    private static void addRaw(ArrayList<Byte> out, Object []file) {
+        for (Byte b : (Byte[])file) out.add((byte)b);
+    }
+
+    private static void addArray(ArrayList<Byte> out, Object[] array, ArrayAdder arrayAdder) {
+        int size = array.length;
+        out.add((byte)(size&0xFF));
+        out.add((byte)((size>>8)&0xFF));
+
+        if (size > 0) arrayAdder.addToArray(out, array);
+    }
+
+    private static void addString(ArrayList<Byte> out, String str) {
+        Byte []arr = new Byte[str.length()];
+        for (int n = 0; n < arr.length; n++) arr[n] = (byte)str.charAt(n);
+        TesterConnector.addArray(out, arr, TesterConnector::addRaw);
+    }
+
+    /* INTERFACES */
     @Override
     public String startServer(ServerStartNotifier onServerStart, ServerErrorNotifier onError, Map[] maps, Plugin[] plugins, ServerType mcType, String version, ConfigFile[] configFiles) throws IOException {
         ArrayList<Byte> message = new ArrayList<>();
-        message.add((byte) 0b000_0_000000000001); // start server header
+
+        // start server header
+        message.add((byte) 0b000_0_0000);
+        message.add((byte) 0b00000001);
+
+        TesterConnector.addArray(message, maps, TesterConnector::addRaw); // TODO
+        TesterConnector.addArray(message, plugins, TesterConnector::addRaw); // TODO
+        TesterConnector.addString(message, mcType.name());
+        TesterConnector.addString(message, version);
+        TesterConnector.addArray(message, configFiles, TesterConnector::addRaw); // TODO
+
         OutputStream out = this.serversManagerSocket.getOutputStream();
         DataOutputStream dos = new DataOutputStream(out);
         dos.write(TesterConnector.toByteArray(message), 0, message.size());
-
-        dos.flush();
         dos.close();
 
         return null;
