5bbf1754936d16fb134be29066736703711ece9d
roger.miranda <roger.miranda@students.salle.url.edu>
2022-10-03 16:41:04 +0200

    Fix: id is short

--- src/com/rogermiranda1000/watchwolf/entities/blocks/Block.java
+++ src/com/rogermiranda1000/watchwolf/entities/blocks/Block.java
@@ -1,11 +1,15 @@
 package com.rogermiranda1000.watchwolf.entities.blocks;
 
 public class Block {
-    protected final int id;
+    protected final short id;
     protected final String name;
 
-    protected Block(int id, String name) {
+    protected Block(short id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    protected Block(int id, String name) {
+        this((short) id, name);
     }
 }
