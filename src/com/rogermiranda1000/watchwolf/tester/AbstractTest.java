8595eaf8f03ab54d5d8616c224bc2a2732b6b88e
roger.miranda <roger.miranda@students.salle.url.edu>
2022-10-10 16:09:20 +0200

    Feature: tasks

--- src/com/rogermiranda1000/watchwolf/tester/AbstractTest.java
+++ src/com/rogermiranda1000/watchwolf/tester/AbstractTest.java
@@ -9,12 +9,15 @@ import org.junit.jupiter.api.extension.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.UUID;
 
 public class AbstractTest implements TestWatcher, // send feedback
         BeforeAllCallback, AfterAllCallback, // open/close server
         ParameterResolver { // send arguments
     private Tester tester;
     private TesterConnector connector;
+
+    private UUID testID;
 
     // TODO move to file
     public final String []serversManagerIP = "127.0.0.1:8000".split(":");
@@ -23,10 +26,10 @@ public class AbstractTest implements TestWatcher, // send feedback
 
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws IOException, InterruptedException {
-        this.getConfigFile(); // TODO
         Socket serversManagerSocket = new Socket(serversManagerIP[0], Integer.parseInt(serversManagerIP[1])); // ServersManager socket
+        this.testID = UUID.randomUUID();
 
-        System.out.println("Starting test for " + serverType.name() + " " + this.serverVersion);
+        System.out.println("Starting test for " + serverType.name() + " " + this.serverVersion + " using ID " + testID.toString());
         this.tester = new Tester(serversManagerSocket, this.serverType, this.serverVersion, new Plugin[]{}, new Map[]{}, new ConfigFile[]{}) // TODO rest of variables
                 .setOnServerError(Tester.DEFAULT_ERROR_PRINT); // TODO report to JUnit
 
@@ -49,23 +52,21 @@ public class AbstractTest implements TestWatcher, // send feedback
     @Override
     public void afterAll(ExtensionContext extensionContext) throws Exception {
         this.tester.close();
+
+        // TODO send 'done' to website
     }
 
     @Override
     public void testSuccessful(ExtensionContext context) {
+        // TODO send 'ok' to website
         System.err.println("Test " + context.getDisplayName() + " succeed");
     }
 
     @Override
     public void testFailed(ExtensionContext context, Throwable cause) {
+        // TODO send 'fail' to website
         System.err.println("Test " + context.getDisplayName() + " failed: " + cause.getMessage());
     }
-
-    /**
-     * Method to override
-     * @return WatchWolf config file
-     */
-    public File getConfigFile() { throw new UnspecifiedConfigFileException(); }
 
     @Override
     public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
@@ -76,4 +77,10 @@ public class AbstractTest implements TestWatcher, // send feedback
     public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
         return this.connector;
     }
+
+    /**
+     * Method to override
+     * @return WatchWolf config file
+     */
+    public File getConfigFile() { throw new UnspecifiedConfigFileException(); }
 }
