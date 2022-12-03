2fe0bb8b3d538b8e7ab810c9a54269c037caebf1
roger.miranda <roger.miranda@students.salle.url.edu>
2022-12-03 21:24:19 +0100

    Feature: test break block

--- test/UserTester.java
+++ test/UserTester.java
@@ -1,6 +1,8 @@
 import com.rogermiranda1000.watchwolf.client.ClientPetition;
 import com.rogermiranda1000.watchwolf.client.MessageNotifier;
 import com.rogermiranda1000.watchwolf.entities.Position;
+import com.rogermiranda1000.watchwolf.entities.blocks.Block;
+import com.rogermiranda1000.watchwolf.entities.blocks.Blocks;
 import com.rogermiranda1000.watchwolf.tester.AbstractTest;
 import com.rogermiranda1000.watchwolf.tester.TesterConnector;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +10,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.io.File;
+import java.time.Duration;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(UserTester.class) // run the tests with the AbstractTest overridden methods
 public class UserTester extends AbstractTest {
@@ -41,6 +47,9 @@ public class UserTester extends AbstractTest {
         connector.removeOnMessage(notifier);
     }
 
+    /**
+     * Break the block at the bottom of the player
+     */
     @ParameterizedTest
     @ArgumentsSource(UserTester.class)
     public void breakBlock(TesterConnector connector) throws Exception {
@@ -48,11 +57,15 @@ public class UserTester extends AbstractTest {
         ClientPetition userPetition = connector.getClientPetition(user);
 
         Position pos = connector.server.getPlayerPosition(user).add(0, -1, 0);
-        System.out.println(pos);
-        System.out.println("Requested to break " + connector.server.getBlock(pos).toString() + "...");
+        Block target_block = connector.server.getBlock(pos);
+        System.out.println("Requested to break " + target_block.toString() + "...");
         userPetition.breakBlock(pos); // TODO what if it's stone?
 
-        Thread.sleep(7_000);
-        System.out.println(connector.server.getBlock(pos));
+        // wait till the block breaks (or timeout)
+        int timeout = 7_000;
+        Instant start = Instant.now();
+        while (connector.server.getBlock(pos).equals(target_block) && Duration.between(start, Instant.now()).toMillis() < timeout);
+
+        assertEquals(Blocks.AIR, connector.server.getBlock(pos));
     }
 }
