95109dbbce400fc3179046a735dd201e27f116b3
roger.miranda <roger.miranda@students.salle.url.edu>
2022-12-17 17:44:56 +0100

    Fix: world files offset

--- src/com/rogermiranda1000/watchwolf/entities/files/WorldFile.java
+++ src/com/rogermiranda1000/watchwolf/entities/files/WorldFile.java
@@ -5,15 +5,11 @@ import java.io.IOException;
 
 public class WorldFile extends ConfigFile {
     public WorldFile(String name, File f) throws IOException {
-        super(name, f, "./");
+        super(name + ".zip", f, name + "/");
     }
 
     public WorldFile(String name, String f) throws IOException {
         this(name, new File(f));
-    }
-
-    public WorldFile(String name) throws IOException {
-        super(name, "./");
     }
 
     public String getWorldName() {
